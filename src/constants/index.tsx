fc2504fae5325aeab995475aa55b9c7c29b343d0
Jose Dirazar <jose_f_dirazar@hotmail.com>
2025-02-09 06:25:58 -0300

    Add photo

--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -10,8 +10,7 @@ export const devsData = [
     role: "ForgeBytesLab DevOps, Web and Mobile Developer",
     description:
       "I am a passionate software developer dedicated to transforming client's ideas into reliable, efficient, and high-quality software.",
-    avatar:
-      "https://media.licdn.com/dms/image/v2/D4D03AQF1Nr-fKSeqPA/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1712756668515?e=1743033600&v=beta&t=HnTixBFbKrhSz3Dxz8awLPb99n1ZH5MG2Y2n2229LD8",
+    avatar: "photo.jpeg",
   },
 ] as const;
 
